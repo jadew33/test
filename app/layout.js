233e7c6973783b6b8155e5eb2b12ff6389026bd2
Jade Wei <77248838+jadew33@users.noreply.github.com>
2023-07-01 00:36:05 +0900

    git commit

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "COMPSA",
-  description: "Queen's Computing Students Association.",
+  description: "Queen's Computing Students Associations",
 };
 
 export default function RootLayout({ children }) {
