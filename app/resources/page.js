4efccb575e0bea86cf81f5257930353d2200731d
Sophia <sophia@perzan.dev>
2023-07-11 10:59:18 -0400

    Gradient, h1, and p completed

--- app/resources/page.js
+++ app/resources/page.js
@@ -1,3 +1,17 @@
+import React from "react";
+import "../styles/resources.scss";
+
 export default function Resources() {
-  return <div>Resources</div>;
+    return (
+        <div className="resources">
+            <div className="content">
+                <h1>RESOURCES</h1>
+                <p>Leave us a message with concerns or feedback.</p>
+                <p>
+                    Note, if you would like us to get back to you personally, please leave your
+                    email!
+                </p>
+            </div>
+        </div>
+    );
 }
