ba54c308da0a8e375fc15ad5aae0c61419d331e5
Sophia <sophia@perzan.dev>
2023-07-13 13:59:08 -0400

    Added media breakpoints to adjust font size

--- app/resources/page.js
+++ app/resources/page.js
@@ -17,7 +17,6 @@ export default function Resources() {
                         link="https://www.instagram.com/compsa/"
                         title="Instagram"
                         icon="🔗"
-                        date="08 / 04 / 2022"
                     ></Link>
                     <Link
                         link="https://www.linkedin.com/company/queenscompsa/"
