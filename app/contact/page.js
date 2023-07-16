1f40e4fde04b0c6f1633435273907f7b0ea6e391
Sophia <sophia@perzan.dev>
2023-07-16 12:49:15 -0400

    Finalized contact grid

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,16 +3,15 @@ export default function Contact() {
         <div className="contact">
             <h1>CONTACT US!</h1>
             <div className="contact-grid">
-                <p className="text-content">Leave us a message with concerns or feedback.</p>
-                <p className="note">
-                    Note, if you would like us to get back to you personally, please leave your
-                    email!
+                <p className="text-content">
+                    Leave us a message with concerns or feedback.<br></br> Note, if you would like
+                    us to get back to you personally, please leave your email!
                 </p>
                 <input className="name-field" type="text" placeholder="Name"></input>
                 <input className="email-field" type="text" placeholder="Email"></input>
                 <textarea className="message-field" placeholder="Message"></textarea>
                 <button className="submit" type="submit">
-                    Submit
+                    Submit Message
                 </button>
                 <img
                     className="map"
