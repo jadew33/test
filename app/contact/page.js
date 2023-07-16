c2843c337f82664a71fe86854dcc20ddaedf1e7f
Sophia <sophia@perzan.dev>
2023-07-16 11:13:19 -0400

    added bg gradient, and titles

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,3 +1,16 @@
 export default function Contact() {
-  return <div>Contact Us</div>;
+    return (
+        <div className="contact">
+            <h1>CONTACT US!</h1>
+            <div className="contact-grid">
+                <p>Leave us a message with concerns or feedback.</p>
+                <p>
+                    Note, if you would like us to get back to you personally, please leave your
+                    email!
+                </p>
+            </div>
+            <h3>LOOKING FOR A SPECIFIC PORTFOLIO?</h3>
+            <div className="portfolio-grid"></div>
+        </div>
+    );
 }
