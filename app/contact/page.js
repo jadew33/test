e2a2ec2bb7778c2199c6fbd77da29f40836b1429
Sophia <sophia@perzan.dev>
2023-07-16 12:18:14 -0400

    Set up contact css grid

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,11 +3,21 @@ export default function Contact() {
         <div className="contact">
             <h1>CONTACT US!</h1>
             <div className="contact-grid">
-                <p>Leave us a message with concerns or feedback.</p>
-                <p>
+                <p className="text-content">Leave us a message with concerns or feedback.</p>
+                <p className="note">
                     Note, if you would like us to get back to you personally, please leave your
                     email!
                 </p>
+                <input className="name-field" type="text" placeholder="Name"></input>
+                <input className="email-field" type="text" placeholder="Email"></input>
+                <textarea className="message-field" placeholder="Message"></textarea>
+                <button className="submit" type="submit">
+                    Submit
+                </button>
+                <img
+                    className="map"
+                    src="https://philiaweb.com/uploads/image/google-maps.png"
+                ></img>
             </div>
             <h3>LOOKING FOR A SPECIFIC PORTFOLIO?</h3>
             <div className="portfolio-grid"></div>
