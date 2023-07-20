1275d11a68c03adac24740fbe744fe89cca4dfed
Daniel Dousek <danieldousek@gmail.com>
2023-07-20 20:13:40 +0200

    added frames

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,5 @@
 import "../styles/about.scss";
+import testImage from "../about/test.jpeg";
 
 export default function About() {
   return (
@@ -20,7 +21,110 @@ export default function About() {
           </section>
         </image>
       </section>
-      <section className='bottom'></section>
+      <section className='bottom'>
+        <div class='title-container'>
+          <div class='title-line'></div>
+          <h6>MEET THE TEAM</h6>
+          <div class='title-line'></div>
+        </div>
+
+        <section className='box-container'>
+          <div class='box'>
+            <div class='box-image'>
+              <img src={testImage} alt='description of image' />
+            </div>
+            <div class='box-content'>
+              <h6>Exucutives</h6>
+              <div>
+                <span class='title'>Title</span>
+              </div>
+              <div>
+                <span class='title'>President:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+
+              <button class='btn'>HomePage</button>
+            </div>
+          </div>
+          <div class='box'>
+            <div class='box-image'></div>
+            <div class='box-content'>
+              <h6>Exucutives</h6>
+              <div>
+                <span class='title'>Title</span>
+              </div>
+              <div>
+                <span class='title'>President:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+
+              <button class='btn'>HomePage</button>
+            </div>
+          </div>
+          <div class='box'>
+            <div class='box-image'></div>
+            <div class='box-content'>
+              <h6>Exucutives</h6>
+              <div>
+                <span class='title'>Title</span>
+              </div>
+              <div>
+                <span class='title'>President:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+
+              <button class='btn'>HomePage</button>
+            </div>
+          </div>
+          <div class='box'>
+            <div class='box-image'></div>
+            <div class='box-content'>
+              <h6>Exucutives</h6>
+              <div>
+                <span class='title'>Title</span>
+              </div>
+              <div>
+                <span class='title'>President:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+              <div>
+                <span class='title'>Title:</span>
+                <span>text</span>
+              </div>
+
+              <button class='btn'>HomePage</button>
+            </div>
+          </div>
+        </section>
+      </section>
     </seciton>
   );
 }
