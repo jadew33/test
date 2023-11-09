af9cb7bfe2c21a7b4644f732adce71f4db0ea41f
Anthony Qiu <aq0335@gmail.com>
2023-11-08 21:50:59 -0500

    fixed link issues

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,11 @@ import Link from "next/link";
 
 export default function CompsaButton({ link, text }) {
   return (
-    <Link href={link} className="compsa-button font-book text-compsa-white">
+    <Link
+      href={link}
+      target="_blank"
+      className="compsa-button font-book text-compsa-white"
+    >
       {text}
     </Link>
   );
