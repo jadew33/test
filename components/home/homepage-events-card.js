23ba464ae85acb9b187b48533302ee5f6e49a997
Jade Wei <77248838+jadew33@users.noreply.github.com>
2024-01-29 15:57:47 -0500

    fix cards

--- components/home/homepage-events-card.js
+++ components/home/homepage-events-card.js
@@ -2,10 +2,10 @@ export default function HomepageEventsCard({ date, month, name, location }) {
   return (
     <div className="homepage-events-card">
       <h1 className="date font-gothamBold">{date}</h1>
-      <h2 className="month font-gothamBold">{month}</h2>
+      <h2 className="month font-medium">{month}</h2>
       <div>
-        <p className="text-compsa-indigo font-medium text-2xl">{name}</p>
-        <p className="font-book text-sm">@ {location}</p>
+        <p className="name text-compsa-indigo font-medium">{name}</p>
+        <p className="location font-book">@ {location}</p>
       </div>
     </div>
   );
